8c67f55a75291b6a4f202c3250f8678da84f01e9
Jon Carey <jon.carey@whitehatsec.com>
2020-09-17 18:04:31 -0500

    Initial commit

---
+++ cmd/ex-client/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+
+	"github.com/whitehatsec/go-lib/pkg/logging"
+	grpcclient "github.com/whitehatsec/go-lib/pkg/transport/grpc/client"
+	service "github.com/whitehatsec/sentinel-api/pkg/grpc/discoveredvulnservice"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ctx := context.Background()
+	logger := logging.NewLogger(&logging.Configuration{
+		LogLevel:      "debug",
+		HumanFriendly: true,
+		MaxStackSize:  4 << 10,
+	})
+	cc, err := grpcclient.NewClientConn(
+		&grpcclient.Configuration{
+			Target:             "localhost:12001",
+			InsecureSkipVerify: true,
+		},
+		grpcclient.WithLogger(logger),
+		grpcclient.WithDialOptions(grpc.WithBlock()),
+	)
+	if err != nil {
+		panic(err)
+	}
+	client := service.NewDiscoveredVulnServiceClient(cc)
+	res, err := client.AddAttackVector(ctx, &service.AddAttackVectorRequest{})
+	logger.Error().Err(err).Interface("res", res).Msg("add av response")
+	stream, err := client.ListAttackVectors(ctx, &service.ListAttackVectorsRequest{})
+	logger.Error().Err(err).Interface("stream", stream).Msg("list av response")
+	msg, err := stream.Recv()
+	logger.Error().Err(err).Interface("msg", msg).Msg("list av message")
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module ex-client
+
+replace github.com/whitehatsec/go-lib => github.com/joncarey-whitehatsec/go-lib v0.0.0-20200917222022-101b56741480
+
+go 1.13
+
+require (
+	github.com/whitehatsec/go-lib v0.0.0-00010101000000-000000000000
+	github.com/whitehatsec/sentinel-api v0.0.0-20200917181720-e13596be7c3f
+	google.golang.org/grpc v1.31.0
+)
